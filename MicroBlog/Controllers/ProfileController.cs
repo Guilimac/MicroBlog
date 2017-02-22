562701a3658cbbd15803fc631cb35902f72b83bd
Guilherme <gui_lima_c@hotmail.com>
2017-02-22 14:29:23 -0300

    profile controller

---
+++ MicroBlog/Controllers/ProfileController.cs
@@ -0,0 +1,34 @@
+﻿using MicroBlog.DAL;
+using MicroBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicroBlog.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            if (Session["Usuario"] != null)
+            {
+                UserViewModel user = (UserViewModel)Session["Usuario"];
+                return View(user);
+            }
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            UserViewModel user = UserDAO.GetById(id);
+            if(user != null)
+            {
+                return View(user);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MicroBlog/DAL/UserDAO.cs
@@ -0,0 +1,44 @@
+﻿using MicroBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroBlog.DAL
+{
+    public static class UserDAO
+    {
+        private static List<UserViewModel> users;
+        
+        public static UserViewModel AddUser(UserViewModel user)
+        {
+            if (users == null)
+            {
+                users = new List<UserViewModel>();
+            }
+
+            user.UserId = Guid.NewGuid();
+            users.Add(user);
+
+            return user;
+        }
+
+        public static UserViewModel Login(string login, string password)
+        {
+            return users.Find(u => u.UserName == login && u.Password == password);
+        }
+
+        public static UserViewModel GetById(Guid id)
+        {
+            return users.Find(u => u.UserId == id);
+            //for(int i = 0; i< users.Count; i++)
+            //{
+            //    if(users[i].UserId == id)
+            //    {
+            //        return users[i];
+            //    }
+            //}
+            //return null;
+        }
+    }
+}
