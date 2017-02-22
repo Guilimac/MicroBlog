a755600f71a51728abe9556b1886117340c748ab
Guilherme <gui_lima_c@hotmail.com>
2017-02-22 12:52:55 -0300

    projeto Microblog

---
+++ MicroBlog/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MicroBlog.ViewModels;
+
+namespace MicroBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Register()
+        {
+            if (TempData["Mensagem"] != null)
+            {
+                ViewBag.Mensagem = TempData["Mensagem"];
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            var teste = user;
+            Session["Usuario"] = user;
+            TempData["Mensagem"] = "Usuário cadastrado com sucesso!";
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ MicroBlog/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using MicroBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicroBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(Session["Usuario"]!= null)
+            {
+                UserViewModel usuario = (UserViewModel)Session["Usuario"];
+                ViewBag.NomeUsuario = usuario.UserName;
+            }
+            if(TempData["Mensagem"] != null)
+            {
+                ViewBag.Mensagem = TempData["Mensagem"];
+                TempData.Keep();
+            }
+            
+            return View();
+        }
+
+    }
+}
